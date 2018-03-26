42f96f0b9ae4117e809aa28c683c079e1651a5c4
Matt Bentrovato <mbentro@gmail.com>
2018-03-26 20:48:32 +0000

    Added form fields

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
+#Need to do Controller, View and Route to get a basic page working...
 Rails.application.routes.draw do
   root to: 'pages#home' #root = home, root needs to point to the pages controller, home method
   get 'about', to: 'pages#about'  #About page link
-  resources :contacts
+  resources :contacts   #Identical to [get 'contacts/new', to: 'contacts#new']
 end

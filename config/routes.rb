fee0ea1e763ff88dfdf66992877bbcda80896ed4
Matt Bentrovato <mbentro@gmail.com>
2018-03-27 18:57:19 +0000

    Added nav link for contact page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home' #root = home, root needs to point to the pages controller, home method
   get 'about', to: 'pages#about'  #About page link
-  resources :contacts   #Identical to [get 'contacts/new', to: 'contacts#new']
+  resources :contacts   #Identical to [get 'contacts/new', to: 'contacts#new'] sortcut to generate new routes (new/edit/show/etc)
 end

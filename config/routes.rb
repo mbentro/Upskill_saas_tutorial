6c01b68bb6fa3f451cdb475720183d7cf2d8478b
Matt Bentrovato <mbentro@gmail.com>
2018-04-19 21:59:11 +0000

    Added custom contact form path

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'pages#home' #root = home, root needs to point to the pages controller, home method
   get 'about', to: 'pages#about'  #About page link
   resources :contacts   #Identical to [get 'contacts/new', to: 'contacts#new'] sortcut to generate new routes (new/edit/show/etc)
+  get 'contact-us', to: 'contacts#new' #point the custom path name (contact-us) to the approriate controller (contacts) and action (new)
 end
